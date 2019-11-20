743a6f4bf87efd0360a9bf7e5df0558af130431c
kanziw <kanziwoong@gmail.com>
2019-11-21 02:57:21 +0900

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+source .macos
+brew bundle
+source zsh.sh
+source git.sh
