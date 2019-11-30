9ae772a4b567baa54242184dcb1535d8e5fb473d
kanziw <kanziwoong@gmail.com>
2019-11-30 16:42:58 +0900

    Make symbolic link work

--- git.sh
+++ git.sh
@@ -5,11 +5,11 @@ brew install git git-lfs
 if [ -f ~/.gitignore_global ]; then
   rm ~/.gitignore_global
 fi
-ln -s .gitignore_global ~/.gitignore_global
+ln -s $(pwd)/.gitignore_global ~/.gitignore_global
 
 if [ -f ~/.gitconfig ]; then
   rm ~/.gitconfig
 fi
-ln -s .gitconfig ~/.gitconfig
+ln -s $(pwd)/.gitconfig ~/.gitconfig
 
 echo "-git"
--- zsh.sh
+++ zsh.sh
@@ -3,12 +3,12 @@ echo "+zsh"
 if [ -f ~/.zshrc ]; then
   rm ~/.zshrc
 fi
-ln -s .zshrc ~/.zshrc
+ln -s $(pwd)/.zshrc ~/.zshrc
 
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-ln -s bin ~/bin
+ln -s $(pwd)/bin ~/bin
 
 # Install Node.js for pure-prompt
 nvm install --lts node
